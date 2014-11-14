3fd93d900095908bed6d8b90e71f193e5b13477e
Alex Kharchenko <isiotmusic@gmail.com>
2014-11-14 12:59:30 +0200

    fix require mongoid files

--- lib/active_admin/mongoid.rb
+++ lib/active_admin/mongoid.rb
@@ -15,7 +15,6 @@ require 'active_admin/mongoid/document'
 require 'active_admin/mongoid/helpers/collection'
 require 'active_admin/mongoid/criteria'
 
-require 'active_admin/mongoid/order_clause'
 require 'active_admin/mongoid/filters/formtastic_addons'
 
 module ActiveAdmin

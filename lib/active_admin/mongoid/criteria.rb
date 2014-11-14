e0fa74d8b3d69bcb63ab038679f2008342fb81bf
Alex Kharchenko <isiotmusic@gmail.com>
2014-11-14 12:56:49 +0200

    fix filter

--- lib/active_admin/mongoid/criteria.rb
+++ lib/active_admin/mongoid/criteria.rb
@@ -1,22 +1,9 @@
-require 'delegate'
-
 Mongoid::Criteria.class_eval do
-  def relation *args, &block
+  def relation(*args, &block)
     self
   end
 
   def base
     klass
   end
-
-  def table
-    CollectionTable.new(klass.collection)
-  end
-
-  class CollectionTable < SimpleDelegator
-    def from(*a)
-      self
-    end
-  end
 end
-
--- lib/active_admin/mongoid/helpers/collection.rb
+++ lib/active_admin/mongoid/helpers/collection.rb
@@ -1,20 +1,10 @@
-module ActiveAdmin
-  module Helpers
-    module Collection
-
-      alias original_collection_size collection_size
-      instance_method(:collection_size)
-      def collection_size(collection = collection)
-        if not collection.empty? and collection.first.class.included_modules.include?(Mongoid::Document)
-          if collection.first.class.embedded?
-            collection.count
-          else
-            collection.count(true)
-          end
-        else
-          collection.count
-        end
-      end
-    end
-  end
-end
+# module ActiveAdmin
+#   module Helpers
+#     module Collection
+#       def collection_size(collection=collection)
+#         raise collection.inspect
+#         collection.count
+#       end
+#     end
+#   end
+# end
